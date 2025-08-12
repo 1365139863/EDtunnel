e65fb89185f9c87ce869c3984542b632ef9b076d
ob <wjc1365139863@outlook.com>
2025-08-12 23:43:32 +0800

    11451411

--- gongneng1-1.c
+++ gongneng1-1.c
@@ -1,1 +1,19 @@
 #include <string.h>
+
+int main() {
+    char str1[] = "Hello, World!";
+    char str2[] = "Hello, World!";
+    
+    // 使用 strcmp 比较两个字符串
+    int result = strcmp(str1, str2);
+    
+    if (result == 0) {
+        // 字符串相等
+    } else if (result < 0) {
+        // str1 小于 str2
+    } else {
+        // str1 大于 str2
+    }
+    
+    return 0;
+}
